47026e45b78def772d24715b69042c4c2ffe92db
Mitch Kahn <mitch@appdelegates.com>
2016-07-27 16:34:34 -0700

    ABService and UDPService cleansed.

--- app/src/main/java/io/ourglass/amstelbright2/services/amstelbright/AmstelBrightService.java
+++ app/src/main/java/io/ourglass/amstelbright2/services/amstelbright/AmstelBrightService.java
@@ -3,19 +3,14 @@ package io.ourglass.amstelbright2.services.amstelbright;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Handler;
 import android.os.IBinder;
-import android.os.Message;
-import android.os.Messenger;
+import android.support.annotation.Nullable;
 import android.util.Log;
-import android.widget.Toast;
 
-import org.json.JSONObject;
-
+import io.ourglass.amstelbright2.core.ABApplication;
 import io.ourglass.amstelbright2.core.OGConstants;
 import io.ourglass.amstelbright2.core.OGCore;
 import io.ourglass.amstelbright2.services.cloudscraper.CloudScraperService;
-import io.ourglass.amstelbright2.services.cloudscraper.OGTweetScraper;
 import io.ourglass.amstelbright2.services.http.HTTPDService;
 import io.ourglass.amstelbright2.services.stbservice.STBService;
 import io.ourglass.amstelbright2.services.udp.UDPBeaconService;
@@ -30,39 +25,36 @@ import io.ourglass.amstelbright2.services.udp.UDPBeaconService;
 
 public class AmstelBrightService extends Service  {
 
-    public static final String TAG = "ABS";
-
-    OGTweetScraper twatter;
-
-    // For debug toasts
-    public static final Boolean DEBUG = false;
-
-    //Context mContext = getApplicationContext();
-    final Messenger mMessenger = new Messenger(new IncomingHandler());
-
     // This is here as a reliable ref to context for the Realm stuff.
     public static Context context;
 
-    public AmstelBrightService() {
-    }
+    public static final String TAG = "ABService";
 
     /** indicates how to behave if the service is killed */
     int mStartMode = START_STICKY;
 
     /** indicates whether onRebind should be used */
-    boolean mAllowRebind = true;
+    //boolean mAllowRebind = true;
+
+    //final Messenger mMessenger = new Messenger(new IncomingHandler());
+
+
+    public AmstelBrightService() {
+    }
+
 
     /** Called when the service is being created. */
     @Override
     public void onCreate() {
-
+        super.onCreate();
+        Log.d(TAG, "In onCreate()");
     }
 
     /** The service is starting, due to a call to startService() */
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
-        dbToastr("ABS: onStartCommand");
+        ABApplication.dbToast(this, "Starting Background Services");
 
         // Create global static ref for code that needs Context
         context = getApplicationContext();
@@ -71,73 +63,18 @@ public class AmstelBrightService extends Service  {
         OGCore.sendStatusIntent("STATUS", "Starting Services",
                 OGConstants.BootState.ABS_START.getValue());
 
-
         startChildServices();
 
         return mStartMode;
     }
 
-    /** A client is binding to the service with bindService() */
-    @Override
-    public IBinder onBind(Intent intent) {
-        dbToastr("ABS: binding");
-        return mMessenger.getBinder();
-    }
-
-    /** Called when all clients have unbound with unbindService() */
-    @Override
-    public boolean onUnbind(Intent intent) {
-        super.onUnbind(intent);
-        dbToastr("ABS: unbinding");
-        return mAllowRebind;
-    }
-
-
-    /** Called when The service is no longer used and is being destroyed */
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        dbToastr("ABS: onDestroy");
-
-    }
-
-
-
-
-    // TODO: Seriously with the leaks? This code is right from the Google site. FCOL
-    private class IncomingHandler extends Handler {
-        @Override
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case 1:
-                    Toast.makeText(getApplicationContext(), "hello!", Toast.LENGTH_SHORT).show();
-                    break;
-                case 2:
-                    Toast.makeText(getApplicationContext(), "beer thirty!", Toast.LENGTH_SHORT).show();
-                    break;
-                default:
-                    super.handleMessage(msg);
-            }
-        }
-    }
-
-    private void logTweets(JSONObject jobj){
-        Log.d(TAG, jobj.toString());
-    }
-
     private void startChildServices(){
-
-
 
         OGCore.installStockApps();
         OGCore.sendStatusIntent("STATUS", "Installing stock apps",
                 OGConstants.BootState.UPGRADE_START.getValue());
 
-
-        Intent udpIntent = new Intent(this, UDPBeaconService.class)
-                .putExtra("data", "some data to broadcast")
-                .putExtra("port", 9091)
-                .putExtra("beaconFreq", 2000);
+        Intent udpIntent = new Intent(this, UDPBeaconService.class);
 
         startService(udpIntent);
 
@@ -152,12 +89,63 @@ public class AmstelBrightService extends Service  {
 
     }
 
-
-    private void dbToastr(String msg){
-
-        if (DEBUG){
-            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
-        }
-
+    /** Called when The service is no longer used and is being destroyed */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "In onDestroy()");
     }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+    // MAK: We aren't binding right now, but it's here for ref
+
+//    /** A client is binding to the service with bindService() */
+//    @Override
+//    public IBinder onBind(Intent intent) {
+//        return mMessenger.getBinder();
+//    }
+//
+//    /** Called when all clients have unbound with unbindService() */
+//    @Override
+//    public boolean onUnbind(Intent intent) {
+//        super.onUnbind(intent);
+//        return mAllowRebind;
+//    }
+
+
+
+
+//
+//
+//
+//    // TODO: Seriously with the leaks? This code is right from the Google site. FCOL
+//    private class IncomingHandler extends Handler {
+//        @Override
+//        public void handleMessage(Message msg) {
+//            switch (msg.what) {
+//                case 1:
+//                    Toast.makeText(getApplicationContext(), "hello!", Toast.LENGTH_SHORT).show();
+//                    break;
+//                case 2:
+//                    Toast.makeText(getApplicationContext(), "beer thirty!", Toast.LENGTH_SHORT).show();
+//                    break;
+//                default:
+//                    super.handleMessage(msg);
+//            }
+//        }
+//    }
+//
+//    private void logTweets(JSONObject jobj){
+//        Log.d(TAG, jobj.toString());
+//    }
+
+
+
+
 }
